efd6d28727f7401977c220cdbe3289f63897f284
SillyMeTimbers <70308288+SillyMeTimbers@users.noreply.github.com>
2023-06-03 16:02:00 -0400

    Update Core.user.js

--- Core.user.js
+++ Core.user.js
@@ -10,190 +10,34 @@
 // ==/UserScript==
 
 const Redirects = [
-  "ExtensionList",
-  "ExpNotes_VerifyTime",
-  "TopBarAlert",
-  "OverdueOpenInPOS",
-  "SendDropoffButtons",
-  "EnhancedMessageTemplates",
-  "HideUncoveredNotDispatch",
-  "ReservationSearch",
-  "VersionManager",
+    "FormatPhoneNumber",
 ];
 
-const PermissionsList = {
-    "Joshua Mccart": {
-        Branch: {
-            Live: false,
-        },
-        Access: {
-            "ExtensionList": true,
-            "ExpNotes_VerifyTime": true,
-            "TopBarAlert": true,
-            "OverdueOpenInPOS": true,
-            "SendDropoffButtons": true,
-            "EnhancedMessageTemplates": true,
-            "HideUncoveredNotDispatch": true,
-            "ReservationSearch": true,
-            "VersionManager": true,
-        },
-    },
-
-    "Emily Bertrand": {
-        Branch: {
-            Live: true,
-        },
-        Access: {
-            "ExtensionList": true,
-            "ExpNotes_VerifyTime": true,
-            "TopBarAlert": true,
-            "OverdueOpenInPOS": true,
-            "SendDropoffButtons": true,
-            "EnhancedMessageTemplates": true,
-            "HideUncoveredNotDispatch": true,
-            "ReservationSearch": true,
-            "VersionManager": true,
-        },
-    },
-
-    "Melissa Wise": {
-        Branch: {
-            Live: false,
-        },
-        Access: {
-            "ExtensionList": true,
-            "ExpNotes_VerifyTime": true,
-            "TopBarAlert": true,
-            "OverdueOpenInPOS": true,
-            "SendDropoffButtons": true,
-            "EnhancedMessageTemplates": true,
-            "HideUncoveredNotDispatch": true,
-            "ReservationSearch": true,
-            "VersionManager": true,
-        },
-    },
-
-    "Julianna Mayes": {
-        Branch: {
-            Live: true,
-        },
-        Access: {
-            "ExtensionList": true,
-            "ExpNotes_VerifyTime": false,
-            "TopBarAlert": true,
-            "OverdueOpenInPOS": true,
-            "SendDropoffButtons": true,
-            "EnhancedMessageTemplates": true,
-            "HideUncoveredNotDispatch": true,
-            "ReservationSearch": true,
-            "VersionManager": true,
-        },
-    },
-
-    "Michelle Asker": {
-        Branch: {
-            Live: true,
-        },
-        Access: {
-            "ExtensionList": true,
-            "ExpNotes_VerifyTime": true,
-            "TopBarAlert": true,
-            "OverdueOpenInPOS": true,
-            "SendDropoffButtons": true,
-            "EnhancedMessageTemplates": true,
-            "HideUncoveredNotDispatch": true,
-            "ReservationSearch": true,
-            "VersionManager": true,
-        },
-    },
-    
-    "Sheryse McKenzie": {
-        Branch: {
-            Live: true,
-        },
-        Access: {
-            "ExtensionList": true,
-            "ExpNotes_VerifyTime": false,
-            "TopBarAlert": true,
-            "OverdueOpenInPOS": true,
-            "SendDropoffButtons": true,
-            "EnhancedMessageTemplates": true,
-            "HideUncoveredNotDispatch": true,
-            "ReservationSearch": true,
-            "VersionManager": true,
-        },
-    },
-    
-    "Nyla Whitty": {
-        Branch: {
-            Live: true,
-        },
-        Access: {
-            "ExtensionList": true,
-            "ExpNotes_VerifyTime": false,
-            "TopBarAlert": true,
-            "OverdueOpenInPOS": true,
-            "SendDropoffButtons": true,
-            "EnhancedMessageTemplates": true,
-            "HideUncoveredNotDispatch": true,
-            "ReservationSearch": true,
-            "VersionManager": true,
-        },
-    },
-    
-    "Wilson Burgos": {
-        Branch: {
-            Live: true,
-        },
-        Access: {
-            "ExtensionList": true,
-            "ExpNotes_VerifyTime": false,
-            "TopBarAlert": true,
-            "OverdueOpenInPOS": true,
-            "SendDropoffButtons": true,
-            "EnhancedMessageTemplates": true,
-            "HideUncoveredNotDispatch": true,
-            "ReservationSearch": true,
-            "VersionManager": true,
-        },
-    },
-};
-
 (async function() {
-  'use strict';
-  const UpdateVal = 1;
-  const now = new Date();
-  const minutes = Math.floor(now.getMinutes() / UpdateVal) * UpdateVal;
-  const timeString = now.getFullYear() + '-' + (now.getMonth() + 1) + '-' + now.getDate() + '-' + now.getHours() + ':' + minutes;
-
-  const userName = dynatraceUserName.textContent;
-  if (userName in PermissionsList) {
-    const userPermissions = PermissionsList[userName];
-    const branch = userPermissions.Branch.Live ? 'Live' : 'Experimental';
-    const githubURL = `https://raw.githubusercontent.com/SillyMeTimbers/WorkspaceAMT/${branch}/`;
+    'use strict';
+    const UpdateVal = 1;
+    const now = new Date();
+    const minutes = Math.floor(now.getMinutes() / UpdateVal) * UpdateVal;
+    const timeString = now.getFullYear() + '-' + (now.getMonth() + 1) + '-' + now.getDate() + '-' + now.getHours() + ':' + minutes;
+    const githubURL = `https://raw.githubusercontent.com/SillyMeTimbers/WorkspaceCisco/main/`;
 
     for (const redirect of Redirects) {
-      if (userPermissions.Access[redirect]) {
         const url = `${githubURL}${redirect}.user.js?time=${timeString}`;
 
         try {
-          const response = await fetch(url);
+            const response = await fetch(url);
 
-          if (response.ok) {
-            const scriptText = await response.text();
-            const script = document.createElement('script');
-            script.textContent = scriptText;
-            document.head.appendChild(script);
-          } else {
-            console.log(`Failed to fetch script: ${redirect}`);
-          }
+            if (response.ok) {
+                const scriptText = await response.text();
+                const script = document.createElement('script');
+                script.textContent = scriptText;
+                document.head.appendChild(script);
+            } else {
+                console.log(`Failed to fetch script: ${redirect}`);
+            }
         } catch (error) {
-          console.log(`An error occurred while fetching script: ${redirect}`);
-          console.error(error);
+            console.log(`An error occurred while fetching script: ${redirect}`);
+            console.error(error);
         }
-      }
     }
-  } else {
-    console.log(`User "${userName}" is not authorized.`);
-  }
 })();
